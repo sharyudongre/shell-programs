bce14fa7d3f646c809fe84c84e1940713431ae07
sharyudongre <53489514+sharyudongre@users.noreply.github.com>
2020-09-01 17:35:14 +0530

    Create add_two_numbers.sh

---
+++ add_two_numbers.sh
@@ -0,0 +1,16 @@
+# 1. ADD TWO NUMBERS
+
+echo "Enter the first number"
+read a
+echo "Enter second number"
+read b
+sum=$(($a+$b))
+echo "Sum of $a and $b is $sum"
+
+#Output
+Enter the first number                                                                                                        
+3                                                                                                                             
+Enter second number                                                                                                           
+5                                                                                                                             
+Sum of 3 and 5 is 8   
+
