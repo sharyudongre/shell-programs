720a076e8a59d436344e97e330a0d52f20137a3c
sharyudongre <53489514+sharyudongre@users.noreply.github.com>
2020-09-01 17:36:23 +0530

    Create multiply_two_numbers.sh

---
+++ multiply_two_numbers.sh
@@ -0,0 +1,17 @@
+# 2. MULTIPLY TWO NUMBERS
+
+echo "Enter the first number"
+read a
+echo "Enter second number"
+read b
+product=$(($a*$b))
+echo "Product of $a and $b is $product"
+
+#Output
+Enter the first number                                                                                                        
+4                                                                                                                             
+Enter second number                                                                                                           
+5                                                                                                                             
+Product of 4 and 5 is 20 
+
+
